7aca192486d73098fa16a863f7122fff61bb65bb
Naturel1 <moi@arnaudhubert.be>
2023-09-25 15:42:11 +0200

    exercice 1.3 finished

--- exercices/1.3/routes/films.js
+++ exercices/1.3/routes/films.js
@@ -27,7 +27,39 @@ const films = [
 
 // Read all the films
 router.get('/', function (req, res) {
-  return res.json(films);
+  if (req.query["minimum-duration"] === undefined) {
+    return res.json(films);
+  } else {
+    return res.json(films.filter(film => film.duration >= req.query["minimum-duration"]));
+  }
 });
 
+// Read one film
+router.get('/:id', function (req, res) {
+  return res.json(films.find(film => film.id === parseInt(req.params.id)));
+});
+
+// Post a new film
+router.post('/', function (req, res) {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const duration = req?.body?.duration?.length!== 0 ? req.body.duration : undefined;
+  const budget = req?.body?.budget?.length!== 0? req.body.budget : undefined;
+  const link = req?.body?.link?.length!== 0? req.body.link : undefined;
+
+  if (!title || !duration || !budget || !link) { return res.status(400)};
+
+  const lastItemIndex = films?.length !== 0 ? films.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? films[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  films.push({
+    id: nextId,
+    title: title,
+    duration: duration,
+    budget: budget,
+    link: link,
+  });
+
+  return res.json(films);
+});
 module.exports = router;
